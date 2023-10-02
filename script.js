89346c400e06bddc6ce6891495a9618273381434
TrymCoder <trymel.koder@gmail.com>
2023-10-02 13:20:05 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+//console.log("hello world");
+
+/* Varialbel vi kan lett forandre på */
+const userName = "trymsenerher";
+ // userName = "trymsenerher";
+console.log(userName);
+
+let information = "Portfolio";
+// information = "Portfolio";
+console.log(information);
+
+let age = 23;
+age = 24;
+console.log(age);
+
+let myage = 23;
+myage = 23;
+console.log(myage);
+
+let mylocation = "Norway";
+mylocation = "Bergen";
+console.log(mylocation);
+
+let myemail = "trymkoder@gmail.com";
+myemail = "trymkoder@gmail.com";
+console.log(myemail);
+
+
+
+
+const isItFriday = false
+console.log(isItFriday);
+const isItMonday = true
+console.log(isItMonday);
+
+const greet = "Good morning";
+// const name = "trymsenerher";
+
+const greeting = greet + " " + userName;
+console.log(greeting);
+
+let numberOne = 10;
+let numberTwo = 3;
+let numberThree = 5;
+let numberFour = 7;
+let sum = numberOne + numberTwo * numberThree + numberFour;
+console.log(sum);
